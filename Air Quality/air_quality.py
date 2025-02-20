ab5415c668328882f630c030d60c0f02e1cf6d4c
MGoyal1903 <mayankgoyal19032000@gmail.com>
2025-02-20 19:27:48 +0530

    Projects

---
+++ Air Quality/air_quality.py
@@ -0,0 +1,82 @@
+import mysql.connector.cursor
+import requests
+import mysql.connector
+from mysql.connector import Error
+
+
+api_key = "0f96f9515ba945611889006560d54509"
+lat = int(input("Enter latitude: "))
+lon = int(input("Enter longitude: "))
+url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}"
+
+config = {
+    'user' : 'root',
+    'password' : 'root',
+    'host' : 'localhost',
+    'database' : 'air_quality'
+}
+
+response = requests.get(url)
+df = response.json()
+print(df)
+
+data = {
+    'aqi' : df['list'][0]['main']['aqi'],
+    'co' : df['list'][0]['components']['co'],
+    'no' : df['list'][0]['components']['no'],
+    'no2' : df['list'][0]['components']['no2'],
+    'o3' : df['list'][0]['components']['o3'],
+    'so2' : df['list'][0]['components']['so2'],
+    'pm2.5' : df['list'][0]['components']['pm2_5'],
+    'pm10' : df['list'][0]['components']['pm10'],
+    'nh3' : df['list'][0]['components']['nh3'],
+
+}
+print(data)
+try:
+    connection = mysql.connector.connect(**config)
+    if connection.is_connected:
+        print("Mysql Connection Successfull")
+        cursor = connection.cursor()
+        create_table_query = """
+Create table if not exists airquality(
+        id int primary key auto_increment,
+        aqi float,
+        co float,
+        no float,
+        no2 float,
+        o3 float,
+        so2 float,
+        pm2_5 float,
+        pm10 float,
+        nh3 float,
+        timestamp timestamp default current_timestamp
+        )"""
+
+        cursor.execute(create_table_query)
+        connection.commit()
+        print("Table Created successfully")
+
+        insert_query = """insert into airquality(aqi,co,no,no2,o3,so2,pm2_5,pm10,nh3)
+        value(%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+
+        record = (data['aqi'],data['co'],data['no'],data['no2'],data['o3'],data['so2'],
+                  data['pm2.5'],data['pm10'],data['nh3'])
+        
+        cursor.execute(insert_query,record)
+        connection.commit()
+        print("Data Inserted successfully")
+
+
+except Error as e:
+    print("Error",e)
+
+finally:
+    if connection.is_connected:
+        cursor.close()
+        connection.close()
+        print("Mysql Connection closed")
+
+
+
+hl
---
+++ Weather App/weatherapp.py
@@ -0,0 +1,100 @@
+import requests
+import pandas as pd
+import sqlite3
+import mysql.connector
+from mysql.connector import Error
+
+api_key = "3ac3278b91eea6d1f40291bbe6980adb"
+city = 'New York'
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+
+response = requests.get(url)
+if response.status_code == 200:
+    weather_data = response.json()  # This is a Python dictionary
+    print("Data extraction successful!")
+else:
+    print("Error fetching data:", response.status_code)
+
+data = {
+    'city': city,
+    'description': weather_data['weather'][0]['description'],
+    'Temprature': weather_data['main']['temp'],
+    'humidity': weather_data['main']['humidity']
+}
+
+df = pd.DataFrame([data])
+print("Transformed data")
+print(df)
+
+# conn = sqlite3.connect('weather_data.db')
+# cursor = conn.cursor()
+
+# cursor.execute('''
+#     CREATE TABLE IF NOT EXISTS weather (
+#                id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                city TEXT,
+#                description TEXT,
+#                temprature REAL,
+#                humidity INTEGER,
+#                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)
+# ''')
+
+# conn.commit()
+
+# cursor.execute('''
+# INSERT INTO weather (city, description, temprature, humidity) VALUES (?,?,?,?)''',
+# (data['city'], data['description'], data['Temprature'], data['humidity']))
+
+# conn.commit()
+
+# print("Data loaded into database successfully!")
+
+# conn.close()
+
+
+config = {
+    'user' : 'root',
+    'password' : 'root',
+    'host' : 'localhost',
+    'database' : 'weather_data'
+
+}
+
+try:
+    connection = mysql.connector.connect(**config)
+
+    if connection.is_connected:
+        print("Connection Successfull")
+        cursor = connection.cursor()
+
+        create_table_query ="""
+CREATE TABLE IF NOT EXISTS weather(
+                              id integer primary key auto_increment,
+                              city text,
+                              description text,
+                              temperature int,
+                              humidity integer,
+                              timestamp datetime default current_timestamp
+                            )"""
+        
+        cursor.execute(create_table_query)
+        connection.commit()
+        print("Table weather is ready")
+
+        insert_query = """INSERT INTO weather (city, description, temperature, humidity)
+        VALUES (%s, %s, %s, %s)"""
+
+        record = (data['city'], data['description'], data['Temprature'], data['humidity'])
+
+        cursor.execute(insert_query, record)
+        connection.commit()
+        print("Data Inserted successfully")
+
+except Error as e:
+    print("Error",e)
+
+finally:
+    if connection.is_connected:
+        cursor.close()
+        connection.close()
+        print("Mysql connection closed")
